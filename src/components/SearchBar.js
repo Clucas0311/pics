52516ad8ee14142f744b0d3a70b581e9769a4071
Clucas0311 <clucas0311@gmail.com>
2020-11-21 21:55:48 -0500

    edit the onchange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,8 @@ import React from 'react';
 class SearchBar extends React.Component {
 	// Whenever someone calls the input this method will be called
 	// event object-->  onChange Called everytime someone changes text in the input
-	onInputChange(event) {
-		console.log(event.target.value);
-	}
+	// initializw state set to properties
+	state = { term: '' };
 
 	render() {
 		return (
@@ -14,7 +13,11 @@ class SearchBar extends React.Component {
 					<div className="field">
 						<label>Image Search</label>
 						{/* // When we pass a method to a prop we don't put parens -- we only want the ref */}
-						<input type="text" onChange={this.onInputChange} />
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={(e) => this.setState({ term: e.target.value })}
+						/>
 					</div>
 				</form>
 			</div>
