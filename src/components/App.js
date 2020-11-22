02702821ba015be05a91dcbae2857d125b26565f
Clucas0311 <clucas0311@gmail.com>
2020-11-22 18:41:59 -0500

    Data fetching

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,26 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+	// intialize state to an empty array or object
+	state = { images: [] };
 	// take the search term and allow it to load images etc
-	onSearchSubmit(term) {
+	onSearchSubmit = async (term) => {
 		// accessing photos from api unsplash
 		// make a request to unsplash api
-		axios
-			.get('https://api.unsplash.com/search/photos', {
-				params: { query: term },
-				headers: {
-					Authorization: 'Client-ID GVcaftRqh3cVxQQlgauomURMUy8OJoZGyDPvRtZgEZw'
-				}
-			})
-			.then((response) => {
-				console.log(response.data.results);
-			});
-	}
+		const response = await axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization: 'Client-ID GVcaftRqh3cVxQQlgauomURMUy8OJoZGyDPvRtZgEZw'
+			}
+		});
+		this.setState({ images: response.data.results });
+	};
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}>
 				<SearchBar onSubmit={this.onSearchSubmit} />
+				{/* // print out number of images that we fetched  */}
+				Found: {this.state.images.length} images
 			</div>
 		);
 	}
