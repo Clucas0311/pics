756ba748e893c9438e342b022e6a2bdc555ad27b
Clucas0311 <clucas0311@gmail.com>
2020-11-23 18:34:33 -0500

    List of images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const ImageList = (props) => {
-	const images = props.images.map((image) => <img src={image.urls.regular} />);
+	// Create an array of the images found during search
+	// all arrays need a key
+	const images = props.images.map((image) => <img alt={image.description} key={image.id} src={image.urls.regular} />);
 	return <div>{images}</div>;
 };
 
