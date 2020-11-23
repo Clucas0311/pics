a11056982a3b199fcc7e7f1b6f5e136934a28d46
Clucas0311 <clucas0311@gmail.com>
2020-11-23 18:10:41 -0500

    Creating an image component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
 	// intialize state to an empty array or object
@@ -20,7 +21,7 @@ class App extends React.Component {
 			<div className="ui container" style={{ marginTop: '10px' }}>
 				<SearchBar onSubmit={this.onSearchSubmit} />
 				{/* // print out number of images that we fetched  */}
-				Found: {this.state.images.length} images
+				<ImageList images={this.state.images} />
 			</div>
 		);
 	}
