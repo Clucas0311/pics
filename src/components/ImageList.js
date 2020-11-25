addc14c0f0fc15e4a997c91f0bb8fde7d8f1b7c6
Clucas0311 <clucas0311@gmail.com>
2020-11-24 21:45:07 -0500

    Creating CSS File  for spacing

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = (props) => {
 	// Create an array of the images found during search
 	// all arrays need a key
 	const images = props.images.map((image) => <img alt={image.description} key={image.id} src={image.urls.regular} />);
-	return <div>{images}</div>;
+	return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
