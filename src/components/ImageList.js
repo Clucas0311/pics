b3098ed7005c61357b203de97a765156db2714be
Clucas0311 <clucas0311@gmail.com>
2020-11-24 22:15:49 -0500

    Condensing and creating image card

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,13 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
 	// Create an array of the images found during search
 	// all arrays need a key
-	const images = props.images.map((image) => <img alt={image.description} key={image.id} src={image.urls.regular} />);
+	const images = props.images.map((image) => {
+		return <ImageCard key={image.id} image={image} />;
+	});
 	return <div className="image-list">{images}</div>;
 };
 
